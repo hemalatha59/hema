efbb734f5d5d2002003a261c49895013de8096f2
hemalatha59 <59439997+hemalatha59@users.noreply.github.com>
2020-01-17 20:35:58 -0800

    Add files via upload

---
+++ loan.js
@@ -0,0 +1,13 @@
+function avail(){
+	var limit=100000;
+	var amount=50000;
+	if(amount<limit)
+	{
+
+		document.writeln("loan is available");
+	}
+	else
+	{
+		document.writeln("Not available");
+	}
+}
